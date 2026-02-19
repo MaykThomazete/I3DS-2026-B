9bf9898ca8d29e18ee293da02a189d3c527bc881
maykthomazete <Mayk.thomazete@aluno.senai.br>
2026-02-19 17:05:18 -0300

    feat: implementar busca de filmes utilizando a API OMDB e adicionar arquivo de exemplo .env

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import logo from "./assets/devflix.png"
@@ -6,6 +7,24 @@ import lupa from "./assets/search.svg"
 import Rodape from "./components/rodape/Rodape";
 
 const App = () => {
+  const {movie, setMovies} = useState([]);
+  //Utilizando uma CHAVE de API do arquivo .env
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+//Criando a conexão com a API e trazendo informações
+const searchMovies = async (title) => {
+  const Response = await fetch(`${apiUrl}&s=${title}`);
+  const data = await Response.json;
+
+  //Alimentando a variavel movies
+  setMovies(data.Search);
+};
+
+useEffect( () => {
+  searchMovies("Batman")
+})
+
   return (
     <div id="App">
       <img className="logo" src={logo} alt="Logotipo da plataforma de streaming Devflix em destaque, com letras vermelhas e pretas, promovendo conteúdo diversificado em streaming." />
@@ -20,6 +39,9 @@ const App = () => {
 
 </div>
 
+
+
+<div className="container"></div>
 
 
       <Rodape link={"https://github.com/MaykThomazete"}>MaykThomazete</Rodape>
